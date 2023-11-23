ee225ae3a01f4e8d1f39a641c43a68bcc281420e
exile213 <exilegamer3@gmail.com>
2023-11-24 01:11:14 +0800

    Update GridBox Creation Script(not fixed yet)

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 //grid creation variable
 
-
+let preval = 1;
 //right container variable
 var rightCont = document.getElementById("rightContainer")
 
@@ -10,12 +10,25 @@ var buttontry = document.getElementById("try");
 var sliderval  = document.getElementById("gridslider");
 
 sliderval.addEventListener("input", function(){
-    for(let i=1; i<= sliderval.value;i++){
-        var gridBox = document.createElement("div");
-        gridBox.classList.add("gridBox");
-        rightCont.appendChild(gridBox);
-        console.log("created grid");
+    if(preval < sliderval.value){
+            for(let i=preval; i<= sliderval.value;i++){
+                var gridBox = document.createElement("div");
+                gridBox.classList.add("gridBox");   
+                rightCont.appendChild(gridBox);
+                console.log(sliderval.value);
+                
+            }
+            preval = sliderval.value;
+    }else if(preval >= sliderval.value){
+             var gridBox = Array.from(document.getElementsByClassName("gridBox"));
+            for(let i=preval; i>= sliderval.value;i--){
+                if(i!=1){
+                    rightCont.removeChild(gridBox[i-1]);
+                }
+                console.log(sliderval.value);
+                
+            }
+            preval = sliderval.value;
     }
-
 });
 
