0402b6c2167c57bc3184bd0dd381c87f21624023
exile213 <exilegamer3@gmail.com>
2024-01-21 13:45:58 +0800

    Change and Update GridBox Creation Logic
    -Finalized Grid Creation Js Script
    -Removed Gridboxes size
    -change html input range to maximum 25

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 
 //grid creation variable
-let preval = 1;
-//right container variable
 var rightCont = document.getElementById("rightContainer");
 var gridtext = document.getElementById("gridNum");
-
-var buttontry = document.getElementById("try");
-
 var sliderval  = document.getElementById("gridslider");
-
 
 sliderval.addEventListener("input", function(){
 
     gridtext.textContent=sliderval.value;
+    
     rightCont.innerHTML = "";
-    for(let i =1; i<=sliderval.value;i++){
+
+    rightCont.style.gridTemplateColumns = "repeat("+ sliderval.value + ",1fr)";
+    rightCont.style.gridTemplateRows = "repeat("+ sliderval.value + ",1fr)";
+    
+
+    // creates div grid boxes and fills the columns and rows
+    for(let i =1; i<=sliderval.value*sliderval.value;i++){
         var newBox = document.createElement("div");
         newBox.classList.add("gridBox");
         rightCont.appendChild(newBox);
-
-
     }
 });
 
