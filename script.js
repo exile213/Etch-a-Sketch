ad1d71e5dfe79d23e00010716aa89554352fb52d
exile213 <exilegamer3@gmail.com>
2024-01-21 12:45:00 +0800

    Rewrote Whole JS gridBox Creation Logic
    Also Change CSS for options side,

--- script.js
+++ script.js
@@ -1,34 +1,25 @@
 
 //grid creation variable
-
 let preval = 1;
 //right container variable
-var rightCont = document.getElementById("rightContainer")
+var rightCont = document.getElementById("rightContainer");
+var gridtext = document.getElementById("gridNum");
 
 var buttontry = document.getElementById("try");
 
 var sliderval  = document.getElementById("gridslider");
 
+
 sliderval.addEventListener("input", function(){
-    if(preval < sliderval.value){
-            for(let i=preval; i<= sliderval.value;i++){
-                var gridBox = document.createElement("div");
-                gridBox.classList.add("gridBox");   
-                rightCont.appendChild(gridBox);
-                console.log(sliderval.value);
-                
-            }
-            preval = sliderval.value;
-    }else if(preval >= sliderval.value){
-             var gridBox = Array.from(document.getElementsByClassName("gridBox"));
-            for(let i=preval; i>= sliderval.value;i--){
-                if(i!=1){
-                    rightCont.removeChild(gridBox[i-1]);
-                }
-                console.log(sliderval.value);
-                
-            }
-            preval = sliderval.value;
+
+    gridtext.textContent=sliderval.value;
+    rightCont.innerHTML = "";
+    for(let i =1; i<=sliderval.value;i++){
+        var newBox = document.createElement("div");
+        newBox.classList.add("gridBox");
+        rightCont.appendChild(newBox);
+
+
     }
 });
 
