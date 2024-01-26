3c188528586193f1dd9fdd01bfaed783871fd20f
exile213 <exilegamer3@gmail.com>
2024-01-26 16:10:06 +0800

    Add Active button Function

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 //Global Variables
-let colorVar = "";
+
+//grid creation variables
 let rightCont = document.getElementById("rightContainer");
 let gridtext = document.getElementById("gridNum");
 let sliderval  = document.getElementById("gridslider");
+
+//Button variables
+let buttonActive=0;
+let colorBut = document.getElementById("colorBut");
+let randColorBut = document.getElementById("randColorBut");
+let eraseBut = document.getElementById("eraseBut");
+let clearBut =  document.getElementById("clearBut");
+
+
+//Colors Variables
 let colorPicker = document.getElementById("colorPicker");
+let colorVar = "";
+
 
 //Predefined function calls
 changeCol();
@@ -15,14 +28,43 @@ changeCol();
 // if user uses the slider
 sliderval.addEventListener("input", function(){
         createGrid();
-});//----------------------------------------------
+});
 
 //if user uses color picker
 colorPicker.addEventListener("input",function(){
     //sets the color of the color picker to a variable
     colorVar = colorPicker.value;
+});
 
-});//---------------------------------------------------------
+//if user clicks on the color button
+colorBut.addEventListener('click',function(){
+    //makes the button remain active after click
+    buttonActive =1;
+    activeButton();
+});
+
+//if user clicks on the random color button
+randColorBut.addEventListener('click',function(){
+    //makes the button remain active after click
+    buttonActive =2;
+    activeButton();
+});
+
+//if user clicks on the erase button
+eraseBut.addEventListener('click',function(){
+    //makes the button remain active after click
+    buttonActive =3;
+    activeButton();
+});
+
+//if user clicks on the erase button
+clearBut.addEventListener('click',function(){
+    //makes the button remain active after click
+    buttonActive =4;
+    activeButton();
+});
+
+
 
 
 
@@ -66,3 +108,37 @@ function changeCol() {
  }
 
 //----------------------------------------------------------------------
+
+
+//BUTTON ACTIVE FUNCTION
+function activeButton() {
+    switch(buttonActive){
+        case 1:
+            colorBut.className = "buttons-active";
+            randColorBut.className ="buttons";
+            eraseBut.className ="buttons";
+            clearBut.className ="buttons";
+            break;
+        
+        case 2:
+            colorBut.className = "buttons";
+            randColorBut.className ="buttons-active";
+            eraseBut.className ="buttons";
+            clearBut.className ="buttons";
+            break;
+
+        case 3:
+            colorBut.className = "buttons";
+            randColorBut.className ="buttons";
+            eraseBut.className ="buttons-active";
+            clearBut.className ="buttons";
+            break;
+        
+        case 4:
+            colorBut.className = "buttons";
+            randColorBut.className ="buttons";
+            eraseBut.className ="buttons";
+            clearBut.className ="buttons-active";
+            break;
+    }
+}
